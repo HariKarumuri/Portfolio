7c0d854c860a03c5e242fa30fa4a563da0a7d9c1
hari <harikarumuri1086@gmail.com>
2022-04-22 16:37:59 +0530

    Updates

---
+++ precache-manifest.e091424cec0c20f5569f87306b579fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3be592a919bfa432bb03",
+    "url": "/Portfolio/static/css/main.2d1880a1.chunk.css"
+  },
+  {
+    "revision": "3be592a919bfa432bb03",
+    "url": "/Portfolio/static/js/main.3be592a9.chunk.js"
+  },
+  {
+    "revision": "cc79fa3910fc97852656",
+    "url": "/Portfolio/static/js/1.cc79fa39.chunk.js"
+  },
+  {
+    "revision": "dcc3aa0822395d35c806",
+    "url": "/Portfolio/static/js/2.dcc3aa08.chunk.js"
+  },
+  {
+    "revision": "59b4de9a9f3ce93ca7c2",
+    "url": "/Portfolio/static/js/runtime~main.59b4de9a.js"
+  },
+  {
+    "revision": "753bcfb6b4e3053671638eb62c71d404",
+    "url": "/Portfolio/static/media/image1.753bcfb6.png"
+  },
+  {
+    "revision": "37a2eac1ae37c3bc9d69d122a3432fc3",
+    "url": "/Portfolio/index.html"
+  }
+];
