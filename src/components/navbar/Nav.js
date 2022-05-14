66597e3521d36aab693b80b18535b7b41915a4a6
hari <harikarumuri1086@gmail.com>
2022-05-14 21:35:40 +0530

    added NavLink instead of Link getting activeClassName

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,45 +1,93 @@
-import React,{useState} from 'react'
-import './Nav.css'
-import {Link} from 'react-router-dom';
-
+import React, { useState } from "react";
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-  const[HamShow,setHamShow] = useState(false);
-  
+  const [HamShow, setHamShow] = useState(false);
+
   return (
     <>
-      <section className='navbar-bg'>
-      <nav className="navbar navbar-expand-lg navbar-light ">
-  <div className="container">
-    <a className="navbar-brand" >Hound-Force</a>
-    <button className="navbar-toggler primary-green" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>{setHamShow(!HamShow)}}>
-      <span className="navbar-toggler-icon  "></span>
-    </button>
-    <div className={ `collapse navbar-collapse ${HamShow?"show":""}   `}   >
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-         <Link to="/Portfolio" className="nav-link active pe-4" aria-current="page" >Home</Link> 
-        </li>
-        <li className="nav-item">
-          <Link to='/Portfolio/about' className="nav-link pe-4 " aria-current="page" >About</Link>
-        </li>
-        <li className="nav-item">
-          <Link to='/Portfolio/portfolio' className="nav-link pe-4 " aria-current="page" >Portfolio</Link>
-        </li>
-        <li className="nav-item">
-          <Link to='/Portfolio/contact' className="nav-link pe-4" aria-current="page" >Contact</Link>
-        </li>
-       
-        
-          
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-</section>
+      <section className="navbar-bg">
+        <nav className="navbar navbar-expand-lg navbar-light ">
+          <div className="container">
+            <a className="navbar-brand">Hound-Force</a>
+            <button
+              className="navbar-toggler primary-green"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => {
+                setHamShow(!HamShow);
+              }}
+            >
+              <span className="navbar-toggler-icon  "></span>
+            </button>
+            <div
+              className={`collapse navbar-collapse ${HamShow ? "show" : ""}   `}
+            >
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink
+                    to="/Portfolio/"
+                    exact
+                    className="nav-link pe-4"
+                    aria-current="page"
+                    onClick={() => {
+                      setHamShow(!HamShow);
+                    }}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/Portfolio/about"
+                    className="nav-link pe-4 "
+                    aria-current="page"
+                    activeClassName="active"
+                    onClick={() => {
+                      setHamShow(!HamShow);
+                    }}
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/Portfolio/portfolio"
+                    className="nav-link pe-4 "
+                    aria-current="page"
+                    activeClassName="active"
+                    onClick={() => {
+                      setHamShow(!HamShow);
+                    }}
+                  >
+                    Portfolio
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/Portfolio/contact"
+                    className="nav-link pe-4"
+                    aria-current="page"
+                    activeClassName="active"
+                    onClick={() => {
+                      setHamShow(!HamShow);
+                    }}
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
