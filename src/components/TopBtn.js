0b85abb1bf7146b445ad8ca6131019e82e1c7059
hari <harikarumuri1086@gmail.com>
2022-05-31 23:18:22 +0530

    removed link scroll and added function scroll

--- src/components/TopBtn.js
+++ src/components/TopBtn.js
@@ -5,11 +5,13 @@ import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 const TopBtn = () => {
   return (
     <div className="container-fluid bg1 arrowContainer">
-      <a href="#home">
-        <i className="topArrow">
+      
+        <i className="topArrow" onClick={()=>{
+          window.scroll(0,0);
+        }}>
           <FontAwesomeIcon icon={faArrowUp} />
         </i>
-      </a>
+      
     </div>
   );
 };
