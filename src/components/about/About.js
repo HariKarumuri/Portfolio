1553576f28978afe45da1437b75370f2ce6560fb
hari <harikarumuri1086@gmail.com>
2022-12-26 16:25:12 +0530

    added the rotating sphere

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,16 +1,62 @@
 import React from "react";
 import "./About.css";
+import { useEffect } from "react";
+import TagCloud from "TagCloud";
 
 const About = () => {
+  
+
+  useEffect(() => {
+    return () => {
+      const container = ".tagcloud";
+      const texts = [
+        "HTML",
+        "CSS",
+        "C",
+        "JavaScript",
+        "React",
+        "Java",
+        "python",
+        "NodeJS",
+        "R",
+        "SQL",
+        "Data structure",
+        "GIT",
+        "GITHUB",
+        "express",
+        "MongoDB",
+        "Spline-3d",
+        "Fire-base",
+        "sanity",
+      ];
+
+      const options = {
+        radius: 350,
+        maxSpeed: "normal",
+        initSpeed: "normal",
+        keep: true,
+      };
+
+      TagCloud(container, texts, options);
+    };
+  }, []);
+
   return (
-    <div className="bg1 py-3 about ">
-      <div className="container ">
-        <h6 className="text-white text-center" id="about">
-          About Me
-        </h6>
-        <h4 className="greenFont ">My Skills</h4>
-        <div className="row">
-          <div className="col d-flex flex-column  ">
+    <>
+      <div className="bg1 py-3 about ">
+        <div className="container ">
+          <h6 className="text-white text-center" id="about">
+            About Me
+          </h6>
+          <h4 className="greenFont ">My Skills</h4>
+          <div className="row mt-2">
+            <div className="col-lg-8 col d-flex flex-column  About_Hidden_mobile">
+            <p className="text-white text-center">Coding and Stuff</p>
+              <div className="text-shpere">
+                <span className="tagcloud"></span>
+              </div>
+            </div>
+            <div className="col d-flex flex-column  About_Hidden">
             <p className="text-white">Programmig languages</p>
 
             <ul>
@@ -23,7 +69,7 @@ const About = () => {
               <li>Data structure</li>
             </ul>
           </div>
-          <div className="col d-flex flex-column ">
+          <div className="col d-flex flex-column About_Hidden">
             <p className="text-white">FrameWorks</p>
             <ul>
               <li>html</li>
@@ -36,26 +82,29 @@ const About = () => {
               <li>NPM</li>
               <li>Spline-3d</li>
               <li>Fire-base</li>
+              <li>sanity</li>
             </ul>
           </div>
-          <div className="col d-flex flex-column ">
-            <p className="text-white">others</p>
-            <ul>
-              <li>blender-3d</li>
-              <li>adobe xd</li>
-              <li>figma</li>
-              <li>canva</li>
-              <li>Adobe Premiere Pro</li>
-              <li>Adobe photoshop</li>
-              <li>Adobe color</li>
-              <li>Ubuntu</li>
-              <li>bootstrap</li>
-              <li>unity</li>
-            </ul>
+            <div className="col-lg-4 col d-flex flex-column ">
+              <p className="text-white text-center">others</p>
+              <ul>
+                <li>blender-3d</li>
+                <li>adobe xd</li>
+                <li>figma</li>
+                <li>canva</li>
+                <li>Adobe Premiere Pro</li>
+                <li>Adobe photoshop</li>
+                <li>Adobe color</li>
+                <li>Ubuntu</li>
+                <li>bootstrap</li>
+                <li>unity</li>
+                <li>postman</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
